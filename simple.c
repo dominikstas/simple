b212341a43b1fdd8c1078279a1b5cffadb923fdc
Dominik Stasiak <d.stasiak436@gmail.com>
2024-06-05 15:37:10 +0200

    Add new commands (pwd, rm, touch) to command-line interface
    - Added `pwd` command to print the current working directory.
    - Added `rm [file]` command to remove a file.
    - Added `touch [file]` command to create an empty file.
    - Updated the help function to include the new commands.
    - Improved error handling and user feedback for each command.

--- simple.c
+++ simple.c
@@ -1,7 +1,7 @@
 #include "simple.h"
 
 void execute(char *command) {
-    if (strncmp(command, "echo ", 4) == 0) {
+    if (strncmp(command, "echo ", 5) == 0) {
         char *text = command + 5; 
         if (strlen(text) > 0) {
             echo(text);
@@ -32,6 +32,26 @@ void execute(char *command) {
         if(mkdir(dir, 0777) != 0) {
             perror("mkdir");
         }
+    } else if (strcmp(command, "pwd") == 0) {
+        char cwd[1024];
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+            printf("%s\n", cwd);
+        } else {
+            perror("pwd");
+        }
+    } else if (strncmp(command, "rm ", 3) == 0) {
+        char *file = command + 3;
+        if(remove(file) != 0) {
+            perror("rm");
+        }
+    } else if (strncmp(command, "touch ", 6) == 0) {
+        char *file = command + 6;
+        FILE *fp = fopen(file, "w");
+        if (fp == NULL) {
+            perror("touch");
+        } else {
+            fclose(fp);
+        }
     } else {
         printf("Unknown command: %s\n", command);
     }
@@ -54,6 +74,9 @@ void help() {
     printf("cd [directory] - change directory\n");
     printf("ls - list directory contents\n");
     printf("mkdir [directory] - create a directory\n");
+    printf("pwd - print working directory\n");
+    printf("rm [file] - remove a file\n");
+    printf("touch [file] - create an empty file\n");
     printf("exit - exit the terminal\n");
     printf("--------------------------------------\n");
 }
